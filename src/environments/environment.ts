2b9ce39ed0bd772e0474efc5cd2b3dc9dcfda084
EzequielMelo <ezequielanthonymelo@gmail.com>
2024-10-10 00:16:51 -0300

    nueva prueba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
-    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
-    appId: process.env['NG_APP_FIREBASE_APP_ID'],
-    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
-    apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
-    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
-    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
+    projectId: 'FIREBASE_PROJECT_ID',
+    appId: 'FIREBASE_APP_ID',
+    storageBucket: 'FIREBASE_STORAGE_BUCKET',
+    apiKey: 'FIREBASE_API_KEY',
+    authDomain: 'FIREBASE_AUTH_DOMAIN',
+    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
   },
   apiQuiz: {
-    apiKey: process.env['NG_APP_API_QUIZ_KEY'],
+    apiKey: 'API_QUIZ_KEY',
   },
 };
